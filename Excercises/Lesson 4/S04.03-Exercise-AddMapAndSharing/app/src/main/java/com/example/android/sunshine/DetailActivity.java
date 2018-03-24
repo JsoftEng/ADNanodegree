26f22fecd7f2091f2baa5765ccf3715e910e8cf4
John Johnson <jsofteng@gmail.com>
2018-03-24 18:17:11 -0400

    completed lesson 4 - sunshine exercises 1, 2 and 3

---
+++ Excercises/Lesson 4/S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,64 @@
+package com.example.android.sunshine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private String mForecast;
+    private TextView mWeatherDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_display_weather);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if (intentThatStartedThisActivity != null) {
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mWeatherDisplay.setText(mForecast);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.details,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_share){
+            shareForecast();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void shareForecast(){
+        String mimeType = "text/plain";
+        String title = "Share Weather Forecast";
+
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setChooserTitle(title)
+                .setType(mimeType)
+                .setText(mForecast)
+                .startChooser();
+    }
+}
